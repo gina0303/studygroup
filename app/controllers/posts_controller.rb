f06ecd064860549597821572ce7643d0894400f4
user <user@userdeMacBook-Air.local>
2015-06-24 17:22:40 +0800

    Added all the things

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
 end
